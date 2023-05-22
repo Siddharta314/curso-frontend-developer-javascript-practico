145ec748c688c005668e265d43912701c9e2fd97
emilio <emilio_k88@hotmail.com>
2023-05-22 09:36:07 +0200

    añadida aside menu y toggle de los 3 componentes

--- main.js
+++ main.js
@@ -4,13 +4,28 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toogleCarritoAside);
 
 function toggleDesktopMenu(){
+    aside.classList.add('inactive'); 
+    
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    aside.classList.add('inactive'); 
+
     mobileMenu.classList.toggle('inactive');
 }
+
+function toogleCarritoAside(){
+    mobileMenu.classList.add('inactive');
+
+    aside.classList.toggle('inactive'); 
+}
+
