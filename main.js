046de31b8d723cb6e1f68b799370754ed6f3a3ad
emilio <emilio_k88@hotmail.com>
2023-05-22 10:49:39 +0200

    añadidos productos en js por lista

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ menuCarritoIcon.addEventListener('click', toogleCarritoAside);
 
 function toggleDesktopMenu(){
     aside.classList.add('inactive'); 
-    
+
     desktopMenu.classList.toggle('inactive');
 }
 
@@ -29,3 +29,51 @@ function toogleCarritoAside(){
     aside.classList.toggle('inactive'); 
 }
 
+
+const productList = [];
+productList.push({
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    name: 'Bike',
+    price: 120
+})
+
+
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1200,
+    image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1600,
+    image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1500,
+    image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push ({
+    name:'Seat',
+    price: 300,
+    image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg'
+});
+
+const cardsContainer = document.querySelector('.cards-container');
+cardsContainer.innerHTML ="";
+
+for(product of productList){
+    const productCard =  `<div class="product-card">
+        <img src=${product.image} alt="">
+        <div class="product-info">
+          <div>
+            <p>$${product.price}</p>
+            <p>${product.name}</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+        </div>
+      </div>`
+    cardsContainer.innerHTML += productCard;
+}
